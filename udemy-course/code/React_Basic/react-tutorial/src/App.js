f6deec0c1586924d232e2db9843d92cf2fc5943e
jayce <jaycethanks@outlook.com>
2022-11-09 19:36:50 +0800

    feat(react-tutoriai): init project again, and papare for adding routing
    初始化项目,准备添加路由

--- udemy-course/code/React_Basic/react-tutorial/src/App.js
+++ udemy-course/code/React_Basic/react-tutorial/src/App.js
@@ -1,54 +1,5 @@
-import { useState } from 'react';
-import Todo from './components/Todo';
-import Modal from './components/Modal';
-import Backdrop from './components/Backdrop';
-
 function App() {
-  const [ifShowModal, setifShowModal] = useState(false);
-  const [todoItems, setTodoItems] = useState([
-    { project: 'Learn React' },
-    { project: 'Master React' },
-    { project: 'Explore the full React Course' },
-  ]);
-
-  const [deleteTargetIndex, setDeleteTargetIndex] = useState(0);
-
-  const deleteHandler = (index) => {
-    setifShowModal(true);
-    setDeleteTargetIndex(null);
-    setDeleteTargetIndex(index);
-  };
-  const confirmHandler = () => {
-    const copy = JSON.parse(JSON.stringify(todoItems));
-    copy.splice(deleteTargetIndex, 1);
-    setTodoItems(copy);
-    setifShowModal(false);
-  };
-  const cancelHandler = () => {
-    setifShowModal(false);
-  };
-  const backdropClickHandler = () => {
-    console.log('[backdropClickHandler]: ', backdropClickHandler);
-    setifShowModal(false);
-  };
-
-  return (
-    <div>
-      <h1>My Todos</h1>
-      {todoItems.map((it, index) => (
-        <Todo
-          key={index}
-          text={it.project}
-          onDelete={() => deleteHandler(index)}
-        />
-      ))}
-
-      {ifShowModal ? (
-        <Modal onConfirm={confirmHandler} onCancel={cancelHandler} />
-      ) : null}
-      {ifShowModal ? <Backdrop onClick={backdropClickHandler} /> : null}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- udemy-course/code/React_Basic/react-tutorial/src/components/Todo.js
+++
@@ -1,9 +0,0 @@
-export default function (props) {
-  const { text } = props;
-  return (
-    <div>
-      <li>todo:{text}</li>
-      <button onClick={props.onDelete}>delete</button>
-    </div>
-  );
-}
